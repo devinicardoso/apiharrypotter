c61020050810b0406f395365eba7e7d9e533231d
Vini Cardoso <vinic8670@gmail.com>
2022-02-18 00:13:56 -0300

    testando

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,14 +1,15 @@
-const preencherFormulario = (endereco) => {
-    document.getEle
-}
+// const preencherFormulario = (endereco) => {
+//     document.getElementById('nome').value = endereco.name
+// }
 
-const pesquisarPersonagem = async() => {
-    const nome = document.getElementById('pesquisar').value
+const pesquisarPersonagem = async () => {
+    // const nome = document.getElementById('pesquisar').value
 
     const url = 'http://hp-api.herokuapp.com/api/characters'
     const dados = await fetch(url)
     const endereco = await dados.json()
-    preencherFormulario(endereco)
+    // preencherFormulario(endereco)
+    console.log(endereco)
 }
 
 function start() {
