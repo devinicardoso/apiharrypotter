c50523328d4c9edb35d8c7895438f2e16733d8c2
Vini Cardoso <vinic8670@gmail.com>
2022-02-24 11:45:19 -0300

    img padrao e search bar

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,22 @@
 const charactersList = document.getElementById("charactersList");
+const searchBar = document.getElementById("searchBar");
+let dados = [];
+
+searchBar.addEventListener("keyup", (e) => {
+    const searchString = e.target.value;
+    const filteredCharacters = dados.filter(characters => {
+        return (
+            characters.name.includes(searchString) || 
+            characters.house.includes(searchString)
+        );
+    });
+    charactersPrint(filteredCharacters);
+})
 
 const loadCharacters = async () => {
     try {
         const url = await fetch('https://hp-api.herokuapp.com/api/characters');
-        const dados = await url.json();
+        dados = await url.json();
         charactersPrint(dados);
         console.log(dados);
     } catch {
@@ -18,15 +31,14 @@ const charactersPrint = (characters) => {
             <li class="character">
                 <div class="text">
                     <h2>${characters.name}</h2>
-                    <p>Data de aniversário: ${characters.dateOfBirth}</p>
                     <p>Casa: ${characters.house}</p>
                 </div>
-                <img src="${characters.image}">
+                <img src="${characters.image}" onerror="this.src = 'assets/img/hogwarts.jpg'">
             </li>
         `;
         })
         .join('');
         charactersList.innerHTML = htmlString;
-    }
+}
 
     loadCharacters();
